cf643c5d0e79dafaae7316af0310cfb8e8886ad1
ilya <dynamokiev1927ilya1@mail.ru>
2020-11-15 14:46:17 +0200

    Initial commit

---
+++ DialogFlowApi.py
@@ -0,0 +1,35 @@
+import dialogflow_v2 as dialogflow
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:\Google_Account\small-talk-sioi-a15d9112b273.json"
+
+
+class DialogFlowApiManager:
+    def __init__(self):
+        pass
+    @staticmethod
+    def GetMessageFromDialogFlowApi(message):
+        sessionClient = dialogflow.SessionsClient()
+        session = sessionClient.session_path("small-talk-sioi", "19881988")
+        text_input = dialogflow.types.TextInput(
+            text=message, language_code='ru')
+
+        query_input = dialogflow.types.QueryInput(text=text_input)
+
+        response = sessionClient.detect_intent(session=session, query_input=query_input)
+        resultMessage=response.query_result.fulfillment_text
+        return resultMessage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ httpclient.py
@@ -0,0 +1,67 @@
+import asyncio
+import aiohttp
+class HttpClient(object):
+    def __init__(self):
+        self.url='https://api.telegram.org/bot1365051067:AAHIxSr2WCPuGqkukq0pHLQCupuEiGA6N3w/'
+
+
+
+    async def get_data_from_server(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://api.telegram.org/bot1365051067:AAHIxSr2WCPuGqkukq0pHLQCupuEiGA6N3w/getUpdates') as response:
+                result=await response.text()
+                return result
+    async def post_data_to_server(self,path,payLoad):
+        fullUrl=self.url+path
+        async with aiohttp.ClientSession() as session:
+
+            await session.post(fullUrl,data=payLoad)
+            
+
+
+
+
+
+async def main():
+
+    print(await HttpClient().get_data_from_server())
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
