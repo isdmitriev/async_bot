e34a1fe3a570f3cd52593dc5c1297e6a12360fb5
ilya <dynamokiev1927ilya1@mail.ru>
2021-01-13 13:51:58 +0200

    last_commit

--- ELIZA/MessageHandler.py
+++ ELIZA/MessageHandler.py
@@ -1,9 +1,6 @@
 from ELIZA.eliza import Eliza
-
 class MessageHandler(object):
     def __init__(self):
         self.eliza_ob=Eliza()
     def get_answer(self,question):
         return self.eliza_ob.respond(question)
-
-
